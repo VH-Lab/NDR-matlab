2c03ee3e7a52bf2ba83d0f5edc28c646099df87a
Yijia <77467551+yijias-65@users.noreply.github.com>
2021-03-22 12:19:40 -0400

    Update test.m

--- +ndr/+test/+format/+spikegadgets/test.m
+++ +ndr/+test/+format/+spikegadgets/test.m
@@ -24,12 +24,12 @@ h,
 
   % will drop word SpikeGadgets in future versions
   
-  [fileconfig, channels] = ndr.format.spikegadgets.read_rec_config(filename,1,0,100) % read first 100 seconds
+  [fileconfig, channels] = ndr.format.spikegadgets.read_rec_config(filename,1,0,100) % not sure how to read first 100 seconds
   
   sr = str2num(fileconfig.samplingRate); 
   
   
-% should I change channel name? 
+% should I change channel name and test for all three channels?
   
   [data, time] = read_rec_trodeChannels(filename,fileconfig.numChannels, channels, sr, fileconfig.headsize,0,100)
 
