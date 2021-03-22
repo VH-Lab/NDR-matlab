d6e8ffe6120e7ea7b543de53bc62b6d491f47e95
Yijia <77467551+yijias-65@users.noreply.github.com>
2021-03-21 23:09:08 -0400

    Update test.m

--- +ndr/+test/+format/+spikegadgets/test.m
+++ +ndr/+test/+format/+spikegadgets/test.m
@@ -23,12 +23,19 @@ disp(['Read header file. Header entries are as follows:']);
 h,
 
   % will drop word SpikeGadgets in future versions
-t = ndr.format.spikegadgets.read_SpikeGadgets_config(filename,'time',1,0,100); % read first 100 seconds
-d = ndr.format.spikegadgets.read_SpikeGadgets_config(filename,'amp',1,0,100); % read first 100 seconds
+  
+  [fileconfig, channels] = ndr.format.spikegadgets.read_rec_config(filename,1,0,100) % read first 100 seconds
+  
+  sr = str2num(fileconfig.samplingRate); 
+  
+  data = read_rec_trodeChannels(filename,header.numChannels,channels-1,sr, header.headerSize,s0,s1);
+  data = read_rec_analogChannels(filename,header.numChannels,byteandbit,sr,header.headerSize,s0,s1)
+  data = read_SpikeGadgets_digitalChannels(filename,header.numChannels,byteandbit,sr,header.headerSize,s0,s1)
+  
 
 if plotit,
 	figure;
-	plot(t,d);
+	plot(time,data);
 	xlabel('Time(s)');
 	ylabel('Data values');
 	title(['SpikeGadgets test data']);
