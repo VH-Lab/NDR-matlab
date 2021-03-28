450e162917f92c00a73fde267cc1b9692f9274e6
Sophie Greer <42726886+Sophie-Greer@users.noreply.github.com>
2021-03-28 19:48:12 -0400

    Add plot

--- +ndr/+test/+reader/+ced_smr/test.m
+++ +ndr/+test/+reader/+ced_smr/test.m
@@ -22,5 +22,12 @@ for i=1:numel(channels),
 	disp(['Channel found (' int2str(i) '/' int2str(numel(channels)) '): ' channels(i).name ' of type ' channels(i).type]);
 end
 
+if plotit,
+	figure (1);
+	plot(t,d);
+	xlabel('Time(s)');
+	ylabel('Data values');
+	title(['CED example data']);
+end;
 
  % okay, here demonstrate use of r.readchannels_epochsamples by reading from channel ai1 and read samples 1 through 10000
