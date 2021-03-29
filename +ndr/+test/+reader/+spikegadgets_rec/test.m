df1fb0889797efb26fae58e06d25699ec4ae509c
Yijia <77467551+yijias-65@users.noreply.github.com>
2021-03-28 22:22:01 -0400

    Update test.m

--- +ndr/+test/+reader/+spikegadgets_rec/test.m
+++ +ndr/+test/+reader/+spikegadgets_rec/test.m
@@ -14,8 +14,13 @@ example_dir = [ndr_globals.path.path filesep 'example_data'];
 
 filename = [example_dir filesep 'example.rec'];
 
-r = ndr.reader('rec'); % open an rec reader
+#r = ndr.reader('rec'); % open an rec reader
 
-channels = getchannelsepoch(ndr_reader_base_spikegadgets_obj, epochfiles)
+r = ndr.reader.spikegadgets.spikegadgets_rec(filename);
+
+channels = ndr.reader.spikegadgets.spikegadgets_rec.getchannelsepoch(ndr_reader_base_spikegadgets_obj, epochfiles);
+
+
+
 
 % okay, here demonstrate use of r.readchannels_epochsamples by reading from channel ai1 and read samples 1 through 10000
