f3812e44299a6230720fe6b262abd3af03e04517
Yijia <77467551+yijias-65@users.noreply.github.com>
2021-04-04 22:19:33 -0400

    Update test.m

--- +ndr/+test/+reader/+spikegadgets_rec/test.m
+++ +ndr/+test/+reader/+spikegadgets_rec/test.m
@@ -18,9 +18,25 @@ filename = [example_dir filesep 'example.rec'];
 r = ndr.reader('rec'); % open an rec reader
 
 
-channels = ndr.reader.spikegadgets.spikegadgets_rec.getchannelsepoch(ndr_reader_base_spikegadgets_obj, epochfiles);
+channels = r.getchannelsepoch({filename});
 
+for i=1:numel(channels),
+  
+  disp(['Channel found (' int2str(i) '/' int2str(numel(channels)) '): ' channels(i).name ' of type ' channels(i).type]);
 
+end
 
 
 % okay, here demonstrate use of r.readchannels_epochsamples by reading from channel ai1 and read samples 1 through 10000
+ 
+ 
+ data = r.readchannels_epochsamples( {filename}, 21, channels, 1,10000)
+ time = r.readchannels_epochsamples(filename,'ai',21,1,10000)
+
+if plotit,
+	figure (1);
+	plot(time,data);
+	xlabel('Time(s)');
+	ylabel('Data values');
+	title(['SpikeGadgets example data']);
+end;
