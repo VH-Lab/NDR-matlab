4b10960fac2f58094a0d4f0738dbe61abd879a39
yijia-65 <yjsun@brandeis.edu>
2021-04-18 21:19:47 -0400

    no epochclock function still work

--- +ndr/+test/+reader/+spikegadgets_rec/test.m
+++ +ndr/+test/+reader/+spikegadgets_rec/test.m
@@ -26,14 +26,23 @@ end
 % okay, here demonstrate use of r.readchannels_epochsamples by reading from channel ai1 and read samples 1 through 10000
  
  epoch_select = 1; 
- channel = 120; % ? number of analog_in
+ channel = 120; 
  
  data = r.readchannels_epochsamples('analog_in',channel,{filename},epoch_select,1,10000);
  time = r.readchannels_epochsamples('time',channel,{filename},epoch_select,1,10000);
- % no time function yet
 
-if plotit, % empty plot shows: vectors must be the same length  
-	figure (1);
+ % each epoch begins at T0 and ends at T1
+ec = r.epochclock({filename}, epoch_select), % ? no epochclock function still work
+t0t1 = r.t0_t1({filename}, epoch_select);
+
+disp(['These are the clocktypes we know and how long the recording lasted:'])
+for i=1:numel(ec),
+    disp(['On clock of type ' ec{i}.ndr_clocktype2char() ' the recording started at ' num2str(t0t1{i}(1)) ' and ended at ' num2str(t0t1{i}(2)) '.']);
+end;
+ 
+
+if plotit, 
+	figure;
 	plot(time,data);
 	xlabel('Time(s)');
 	ylabel('Data values');
