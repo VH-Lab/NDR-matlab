83c7e0afa5f0697a2b4ea3c2cf1b7f4f2af93353
Yijia <77467551+yijias-65@users.noreply.github.com>
2021-04-19 13:44:41 -0400

    Update test.m

--- +ndr/+test/+reader/+spikegadgets_rec/test.m
+++ +ndr/+test/+reader/+spikegadgets_rec/test.m
@@ -32,13 +32,13 @@ end
  time = r.readchannels_epochsamples('time',channel,{filename},epoch_select,1,10000);
 
  % each epoch begins at T0 and ends at T1
-ec = r.epochclock({filename}, epoch_select), % no epochclock function still work, why?
-t0t1 = r.t0_t1({filename}, epoch_select);
+ ec = r.epochclock({filename}, epoch_select), % no epochclock function still work, why?
+ t0t1 = r.t0_t1({filename}, epoch_select);
 
-disp(['These are the clocktypes we know and how long the recording lasted:'])
-for i=1:numel(ec),
-    disp(['On clock of type ' ec{i}.ndr_clocktype2char() ' the recording started at ' num2str(t0t1{i}(1)) ' and ended at ' num2str(t0t1{i}(2)) '.']);
-end;
+ disp(['These are the clocktypes we know and how long the recording lasted:'])
+ 	for i=1:numel(ec),
+		disp(['On clock of type ' ec{i}.ndr_clocktype2char() ' the recording started at ' num2str(t0t1{i}(1)) ' and ended at ' num2str(t0t1{i}(2)) '.']);
+	end;
  
 
 if plotit, 
