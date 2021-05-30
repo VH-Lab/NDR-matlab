cf3c2b0d65cc5963d1ba5422efc83f2e93f07c28
Noah <noah@Noahs-MacBook-Pro.local>
2021-05-29 23:59:41 -0700

    Update intan_rhd.m

--- +ndr/+reader/intan_rhd.m
+++ +ndr/+reader/intan_rhd.m
@@ -23,56 +23,7 @@ classdef intan_rhd < ndr.reader.base
         %  Technologies .RHD file format.
         %
         end % ndr.reader.intan_rhd.intan_rhd
-        
-        function [b,errormsg] = canbereadtogether(intan_rhd_obj, channelstruct)
-        % CANBEREADTOGETHER - Can the channels in a channel struct be read
-        % in a single function call?
-        %
-        %  [B,ERRORMSG] = CANBEREADTOGETHER(INTAN_RHD_OBJ, CHANNELSTRUCT)
-        %
-        %  Returns 1 if the INTAN_RHD_OBJ can read all of the channels in
-        %  CHANNELSTRUCT with a single function call. If they cannot be read
-        %  together, a description is provided in ERRORMSG.
-        %
-        %  In the abstract class, this returns 1 if all of the samplerate
-        %  values are the same and none are NaNs.
-        %
-        % CHANNELSTRUCT is a structure with the following fields:
-        % ------------------------------------------------------------------------------
-        % | Parameter                   | Description                                  |
-	% |-----------------------------|----------------------------------------------|
-	% | internal_type               | Internal channel type; the type of channel as|
-	% |                             |   it is known to the device.                 |
-	% | internal_number             | Internal channel number, as known to device  |
-	% | internal_channelname        | Internal channel name, as known to the device|
-	% | ndr_type                    | The NDR type of channel; should be one of the|
-	% |                             |   types returned by                          |
-	% |                             |   ndr.reader.base.mfdaq_type                 |
-        % | samplerate                  | The sampling rate of this channel, or NaN if |
-	% |                             |   not applicable.                            |
-	% ------------------------------------------------------------------------------
-        %
-            % In the abstract class, this returns 1 if all the samplerates
-            % are the same and none are NaNs.
-            b = 1;
-            errormsg = '';
-            
-            sr = [channelstruct.samplerate];
-            if ~all(isnan(sr)),
-                % if all are not NaN, then none can be
-                if any(isnan(sr)),
-                    b = 0;
-                    errormsg = ['All samplerates must either be the same number or they must all be NaN, indicating they are all not regularly sampled channels.'];
-                else,
-                    sr_ = uniquetol(sr)
-                    if numel(sr_)~=1,
-                        b = 0;
-                        errormsg = ['All sample rates must be the same for all requested regularly-sampled channels for a single function call.'];
-                    end;
-                end;
-            end;
-        end; % ndr.reader.intan_rhd.canbereadtogether
-        
+                
         function channelstruct = daqchannels2internalchannels(intan_rhd_obj, channelprefix, channelnumber, epochstreams, epoch_select)
         % DAQCHANNELS2INTERNALCHANNELS - Convert a set of DAQ channel
         % prefixes and channel numbers to an internal structure to pass to
@@ -98,21 +49,22 @@ classdef intan_rhd < ndr.reader.base
         %  For most devices, EPOCH_SELECT is always 1.
         %
         % Output: CHANNELSTRUCT is a structure with the following fields:
-	% ------------------------------------------------------------------------------
-	% | Parameter                   | Description                                  |
-	% |-----------------------------|----------------------------------------------|
-	% | internal_type               | Internal channel type; the type of channel as|
-	% |                             |   it is known to the device.                 |
-	% | internal_number             | Internal channel number, as known to device  |
-	% | internal_channelname        | Internal channel name, as known to the device|
-	% | ndr_type                    | The NDR type of channel; should be one of the|
-	% |                             |   types returned by                          |
-	% |                             |   ndr.reader.base.mfdaq_type                 |
-	% ------------------------------------------------------------------------------
+        % ------------------------------------------------------------------------------
+        % | Parameter                   | Description                                  |
+        % |-----------------------------|----------------------------------------------|
+        % | internal_type               | Internal channel type; the type of channel as|
+        % |                             |   it is known to the device.                 |
+        % | internal_number             | Internal channel number, as known to device  |
+        % | internal_channelname        | Internal channel name, as known to the device|
+        % | ndr_type                    | The NDR type of channel; should be one of the|
+        % |                             |   types returned by                          |
+        % |                             |   ndr.reader.base.mfdaq_type                 |
+        % ------------------------------------------------------------------------------
         %
-            % abstract class returns empty
-            channelstruct = vlt.data.emptystruct('internal_type','internal_number',...
-                'internal_channelname','ndr_type');
+            filename = intan_rhd_obj.filenamefromepochfiles(epochstreams);
+            header = ndr.format.intan.read_Intan_RHD2000_header(filename);
+            
+            [channelnameprefix, channelnumber] = ndr.string.channelstring2channels(header); % Is this supposed to be in the for loop?
         end; % ndr.reader.intan_rhd.daqchannels2internalchannels
         
         function t0t1 = t0_t1(intan_rhd_obj, epochstreams, epoch_select)
