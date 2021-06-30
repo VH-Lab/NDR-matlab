6e5969ca4829e77d44acc6cd45dc835fd6fcfe3d
Steve Van Hooser <vanhoosr@brandeis.edu>
2021-06-30 11:38:10 -0400

    Update readertest.m

--- +ndr/+test/+reader/+spikegadgets_rec/readertest.m
+++ +ndr/+test/+reader/+spikegadgets_rec/readertest.m
@@ -14,27 +14,7 @@ filename = [example_dir filesep 'example.rec'];
 
 r = ndr.reader('rec'); % open a rec reader
 
-channels = r.getchannelsepoch({filename});
-
-for i=1:numel(channels),
-	disp(['Channel found (' int2str(i) '/' int2str(numel(channels)) '): ' channels(i).name ' of type ' channels(i).type]);
-end
-
-% here, use r.readchannel_epochsamples to create variables d and t
-epoch_select = 1; % which epoch in the file? For most file systems, there is just 1 epoch per file
-channel = 120; % the waveform channel in our example file
-d = r.readchannels_epochsamples('analog_in',120,{filename},epoch_select,1,10000);
-t = r.readchannels_epochsamples('time',120,{filename},epoch_select,1,10000);
-
-% each epoch begins at T0 and ends at T1
-ec = r.epochclock({filename}, epoch_select),
-t0_t1 = r.t0_t1({filename}, epoch_select);
-
-disp(['These are the clocktypes we know and how long the recording lasted:'])
-for i=1:numel(ec),
-	disp(['On clock of type ' ec{i}.ndr_clocktype2char() ' the recording started at ' num2str(t0_t1{i}(1)) ' and ended at ' num2str(t0_t1{i}(2)) '.']);
-end;
-
+[d,t] = r.read({filename}, 'ai120');
 
 if plotit,
 	figure (1);
