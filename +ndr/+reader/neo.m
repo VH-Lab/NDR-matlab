a04a60f0a0e274a10438f3a5ed8233890d91a105
Evgenia Karunus <lakesare@gmail.com>
2022-08-31 01:57:35 +0400

    neo.daqchannels2internalchannels() - implemented initial version

--- +ndr/+reader/neo.m
+++ +ndr/+reader/neo.m
@@ -38,6 +38,27 @@ classdef neo < ndr.reader.base
       % Formatting objects from python to matlab
       data = double(py_data);
     end
+
+    % channelprefix { 'ai', 'ai', 'ai' }
+    % channelnumber [ 21, 120, 5 ]
+    % epochstreams  { '/Users/Me/NDR-matlab/example_data/example.rhd' }
+    % epochselect   1
+    function channelstruct = daqchannels2internalchannels(self, channelprefix, channelnumber, epochstreams, epochselect)
+        py_channels = py.neo_python.convert_channels_from_neo_to_ndi(channelprefix, channelnumber, epochstreams, epochselect);
+
+        % Formatting objects from python to matlab
+        channels = vlt.data.emptystruct('internal_type','internal_number',...
+          'internal_channelname','ndr_type','samplerate');
+        for k = 1:length(py_channels)
+          new_channel.internal_type = char(py_channels{k}{'internal_type'});
+          new_channel.internal_number = char(py_channels{k}{'internal_number'});
+          new_channel.internal_channelname = char(py_channels{k}{'internal_channelname'});
+          new_channel.ndr_type = char(py_channels{k}{'ndr_type'});
+          new_channel.samplerate = char(py_channels{k}{'samplerate'});
+          channels(end + 1) = new_channel;
+        end
+    end;
+
   end
 
   methods (Static)
--- neo_python.py
+++ neo_python.py
@@ -6,53 +6,106 @@ from neo.rawio.spikegadgetsrawio import SpikeGadgetsRawIO
 import quantities as pq
 import numpy as np
 
+class Utils:
+  def get_header_channels(filenames):
+    raw_reader = Utils.get_reader(filenames)
+    raw_reader.parse_header()
+    header = raw_reader.header
+    aa = []
+    for _type in ['signal_channels', 'spike_channels', 'event_channels']:
+      numpy_channels = header[_type]
+      python_channels = [dict(zip(numpy_channels.dtype.names,x)) for x in numpy_channels]
+      for python_channel in python_channels:
+        python_channel['_type'] = _type
+        aa.append(python_channel)
+    return aa
 
-def from_channel_ids_to_stream_index(reader, channel_ids):
-  '''
-  => a single stream_index that all passed channels belong to, or
-  => throws an exception if not all channels belong to a single stream. 
+  # a = get_channels_from_segment(["/Users/lakesare/Desktop/NDR-matlab/example_data/example.rec"], 1, 1)
+  # print(a)
+  def get_channels_from_segment(filenames, segment_index, block_index=1):
+    io_reader = neo.io.get_io(filenames[0])
+    blocks = io_reader.read(lazy=True)
+    block = blocks[int(block_index) - 1]
+    segment = block.segments[int(segment_index) - 1]
 
-  Neo helps us by grouping all analogsignals into a single signal_stream when these analogsignals have the same: "sampling_rate, start_time, length, sample dtype".
-  That is, it only makes sense to retrieve channels from a single stream!
-  
-  Neo docs: "A stream thus has multiple channels which all have the same sampling rate and are on the same clock, have the same sections with t_starts and lengths, and the same data type for their samples. The samples in a stream can thus be retrieved as an Numpy array, a chunk of samples."
-  '''
-  reader.parse_header()
+    signals = segment.analogsignals + segment.spiketrains + segment.irregularlysampledsignals
+    channel_names = []
+    for signal in signals:
+      channel_names += signal.array_annotations['channel_names'].tolist()
 
-  all_channels = reader.header['signal_channels']
-  channels = list(filter(lambda channel: channel['id'] in channel_ids, all_channels))
+    header_channels = Utils.get_header_channels(filenames)
+    our_channels = list(filter(lambda channel: channel['name'] in channel_names, header_channels))
+    return our_channels
 
-  stream_ids = list(map(lambda channel: channel['stream_id'], channels))
+  def from_channel_ids_to_stream_index(reader, channel_ids):
+    '''
+    => a single stream_index that all passed channels belong to, or
+    => throws an exception if not all channels belong to a single stream. 
 
-  unique_stream_ids = np.unique(stream_ids)
+    Neo helps us by grouping all analogsignals into a single signal_stream when these analogsignals have the same: "sampling_rate, start_time, length, sample dtype".
+    That is, it only makes sense to retrieve channels from a single stream!
+    
+    Neo docs: "A stream thus has multiple channels which all have the same sampling rate and are on the same clock, have the same sections with t_starts and lengths, and the same data type for their samples. The samples in a stream can thus be retrieved as an Numpy array, a chunk of samples."
+    '''
+    reader.parse_header()
 
-  if (len(unique_stream_ids) != 1):
-    error_message = ''
-    for channel in channels:
-      error_message += (f"\nChannel_id: '{channel['id']}', stream_id: '{channel['stream_id']}'.")
-    raise ValueError(f"All of your channels should belong to a single signal_stream in Neo.\n{error_message}\n")
-  else:
-    stream_id = unique_stream_ids[0]
-    all_streams = reader.header['signal_streams']
-    for index, stream in enumerate(all_streams):
-      if stream_id == stream['id']: return index
+    all_channels = reader.header['signal_channels']
+    channels = list(filter(lambda channel: channel['id'] in channel_ids, all_channels))
 
-def get_reader(filenames):
-  # NDI passes an array of strings, however Neo always expects a single string, even for multi-file readers.
-  filename = filenames[0]
-  # => e.g. CedRawIO or SpikeGadgetsRawIO
-  Klass = neo.rawio.get_rawio_class(filename)
-  if (Klass.rawmode == 'one-file' or Klass.rawmode == 'multi-file'):
-    reader = Klass(filename=filename)
-  elif (Klass.rawmode == 'one-dir'):
-    reader = Klass(dirname=filename)
-  return reader
+    stream_ids = list(map(lambda channel: channel['stream_id'], channels))
+
+    unique_stream_ids = np.unique(stream_ids)
+
+    if (len(unique_stream_ids) != 1):
+      error_message = ''
+      for channel in channels:
+        error_message += (f"\nChannel_id: '{channel['id']}', stream_id: '{channel['stream_id']}'.")
+      raise ValueError(f"All of your channels should belong to a single signal_stream in Neo.\n{error_message}\n")
+    else:
+      stream_id = unique_stream_ids[0]
+      all_streams = reader.header['signal_streams']
+      for index, stream in enumerate(all_streams):
+        if stream_id == stream['id']: return index
+
+  def get_reader(filenames):
+    # NDI passes an array of strings, however Neo always expects a single string, even for multi-file readers.
+    filename = filenames[0]
+    # => e.g. CedRawIO or SpikeGadgetsRawIO
+    Klass = neo.rawio.get_rawio_class(filename)
+    if (Klass.rawmode == 'one-file' or Klass.rawmode == 'multi-file'):
+      reader = Klass(filename=filename)
+    elif (Klass.rawmode == 'one-dir'):
+      reader = Klass(dirname=filename)
+    return reader
+
+# daqchannels2internalchannels(channelprefix, channelnumber, epochstreams, epochselect)
+def convert_channels_from_neo_to_ndi(channel_prefixes, channel_numbers, filenames, segment_index, block_index=1):
+  # 1. Get all channels from the segment
+  channels = Utils.get_channels_from_segment(filenames, segment_index, block_index)
+  # 2. Filter for the channels we're interested in by channelprefix and channelnumber
+  channel_names = []
+  for i in range(len(channel_prefixes)):
+    channel_names.append(channel_prefixes[i] + channel_numbers[i])
+  needed_channels = filter(lambda channel: channel['name'] in channel_names, channels)
+  # 3. Format from neo to ndi format
+  formatted_channels = list(map(lambda channel: {
+    'internal_type':        channel['_type'],
+    'internal_number':      channel['id'],
+    'internal_channelname': channel['name'],
+    'ndr_type':             'neo',
+    'samplerate':           channel['sampling_rate']
+  }, needed_channels))
+
+  return formatted_channels
+
+# a = convert_channels_from_neo_to_ndi(['Ain'], ['1'], ["/Users/lakesare/Desktop/NDR-matlab/example_data/example.rec"], 1, 1)
+# print(a)
 
 # => [{ type: '', name, '' }, ~]
 def get_channels(filenames, segment_index, block_index=1):
   # 1. If the user cares about all channels in this file, simply parse the header
   if segment_index == 'all':
-    reader = get_reader(filenames)
+    reader = Utils.get_reader(filenames)
     reader.parse_header()
     def format(channels):
       # We can find the Neo type in "mfdaq_prefix(channeltype)" if we like
@@ -82,16 +135,15 @@ def get_channels(filenames, segment_index, block_index=1):
 
     return a + b + c
 
-
-
-get_channels(["/Users/lakesare/Desktop/NDR-matlab/example_data/example.rec"], 0, 0);
+# channels = get_channels(["/Users/lakesare/Desktop/NDR-matlab/example_data/example.rec"], 'all');
+# print(channels)
 
 # readchannels_epochsamples(channeltype, channel, epochfiles, epochselect, s0, s1)
 # Additional arguments: block_index
 def read_channel(channel_type, channel_ids, filenames, segment_index, start_sample, end_sample, block_index=1):
-  reader = get_reader(filenames)
+  reader = Utils.get_reader(filenames)
 
-  stream_index = from_channel_ids_to_stream_index(reader, channel_ids)
+  stream_index = Utils.from_channel_ids_to_stream_index(reader, channel_ids)
   raw = reader.get_analogsignal_chunk(
     block_index=int(block_index), seg_index=int(segment_index),
     i_start=int(start_sample - 1), i_stop=int(end_sample),
@@ -110,7 +162,7 @@ def read_channel(channel_type, channel_ids, filenames, segment_index, start_samp
 
 
 # samplerate(epochstreams, epoch_select, channeltype, channel)
-def sample_rate(filename, epoch_select, channel_type, channel_index):
+def sample_rate(filename, epoch_select, channel_type, channel_id):
   reader = neo.io.get_io(filename)
   blocks = reader.read(lazy=True)
   for block in blocks:
