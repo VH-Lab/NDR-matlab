555cca9a29cc4e9fb92fcb3124fb677dec91e681
Evgenia Karunus <lakesare@gmail.com>
2022-09-06 03:34:04 +0400

    neo.automatedTest.m - add preliminary .read() test

--- +ndr/+test/+reader/+neo/automatedTest.m
+++ +ndr/+test/+reader/+neo/automatedTest.m
@@ -7,12 +7,24 @@ function setupOnce(test_case)
   ndr_Init();
 end
 
+% function test_read(test_case)
+%   filename = utils_get_example('example.rhd');
+%   intan_reader = ndr.reader('intan');
+%   [data, time] = intan_reader.read({ filename }, ['A021+A022'], { 'useSamples', 1, 's0', 1, 's1', 5 });
+%   % channelprefix = ['a', 'a'], channelnumber = [21, 22]
+%   % disp(data);
+%   % disp(time);
+
+%   intan_channels = intan_reader.getchannelsepoch({ filename });
+%   utils_disp_channelstruct()
+% end
+
 function test_getchannelsepoch(test_case)
   filename = utils_get_example('example.rhd');
 
   % Setup intan
   intan_reader = ndr.reader('intan');
-  intan_channels = intan_reader.getchannelsepoch({ filename });
+  intan_channels = intan_reader.getchannelsepoch({ filename }, 1);
 
   % Setup neo
   neo_reader = ndr.reader('neo');
@@ -40,13 +52,26 @@ function test_readchannels_epochsamples(test_case)
   intan_data = intan_reader.readchannels_epochsamples('ai', [ 1, 2 ], { filename }, 1, 1, 10);
   neo_data = neo_reader.readchannels_epochsamples('smth', [ '0', '1' ], { filename }, 1, 1, 10);
 
-  verifyEqual(test_case, intan_data, neo_data, "AbsTol", 0.001)
+  verifyEqual(test_case, intan_data, neo_data, "AbsTol", 0.001);
 end
 
 % Utils
 function utils_disp_channels(channels)
   for i=1:numel(channels),
     disp(['Channel found (' int2str(i) '/' int2str(numel(channels)) '): ' channels(i).name ' of type ' channels(i).type]);
+  end
+end
+
+function utils_disp_channelstruct(channelstruct)
+  for i=1:numel(channelstruct),
+    channel = channelstruct{1}
+    disp([
+      'internal_type: ' channel.internal_type...
+      'internal_number:' channel.internal_number...
+      'internal_channelname:' channel.internal_channelname...
+      'ndr_type:' channel.ndr_type...
+      'samplerate:' channel.samplerate...
+    ])
   end
 end
 
