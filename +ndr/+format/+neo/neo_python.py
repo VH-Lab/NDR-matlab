ac03a8749451bab6cb5bc44101088b7aa14e1e34
Evgenia Karunus <lakesare@gmail.com>
2022-09-07 05:27:56 +0400

    neo_python.py - reorder methods

--- +ndr/+format/+neo/neo_python.py
+++ +ndr/+format/+neo/neo_python.py
@@ -4,62 +4,6 @@ import quantities as pq
 import numpy as np
 import Utils
 
-def t0_t1(filenames, segment_index, block_index=1):
-  reader = neo.io.get_io(filenames[0])
-  block = reader.read()[block_index - 1]
-  segment = block.segments[segment_index - 1]
-
-  def get_magnitude(q):
-    return q.rescale('s').item()
-
-  return [get_magnitude(segment.t_start), get_magnitude(segment.t_stop)]
-
-def daqchannels2internalchannels(channel_names, filenames, segment_index, block_index=1):
-  # 1. Get all channels from the segment
-  channels = Utils.get_channels_from_segment(filenames, segment_index, block_index)
-
-  # 2. Filter for the channels we're interested in
-  needed_channels = filter(lambda channel: channel['name'] in channel_names, channels)
-
-  # 3. Format from neo to ndi format
-  formatted_channels = list(map(lambda channel: {
-    'internal_type':        channel['_type'],
-    'internal_number':      channel['id'],
-    'internal_channelname': channel['name'],
-    'ndr_type':             Utils.channel_type_from_neo_to_ndr(channel['_type']),
-    # TODO hieroglyph prints out if we don't convert to str
-    'samplerate':           str(Utils.channel_to_sample_rate(channel)),
-    # This is a nonstandard Neo-only field
-    'stream_id':            channel['stream_id']
-  }, needed_channels))
-
-  return formatted_channels
-
-def samplerate(filenames, channel_ids):
-  header_channels = Utils.get_header_channels(filenames)
-  our_channels = list(filter(lambda channel: channel['id'] in channel_ids, header_channels))
-  sample_rates = list(map(Utils.channel_to_sample_rate, our_channels))
-  return sample_rates
-
-def canbereadtogether(channelstruct):
-  stream_ids = list(map(lambda channel: channel['stream_id'], channelstruct))
-  unique_stream_ids = np.unique(stream_ids)
-
-  if (len(unique_stream_ids) > 1):
-    error_message = ''
-    for channel in channelstruct:
-      error_message += f"\nChannel_id: '{channel['id']}', stream_id: '{channel['stream_id']}'."
-
-    return {
-      'b': 0,
-      'errormsg': f"All of your channels should belong to a single signal_stream in Neo.\n{error_message}\n"
-    }
-  else:
-    return {
-      'b': 1,
-      'errormsg': ""
-    }
-
 def getchannelsepoch(filenames, segment_index, block_index=1):
   def format(channels):
     return list(map(lambda channel: {
@@ -102,6 +46,62 @@ def readchannels_epochsamples(channel_type, channel_ids, filenames, segment_inde
     )
 
     return rescaled
+
+def daqchannels2internalchannels(channel_names, filenames, segment_index, block_index=1):
+  # 1. Get all channels from the segment
+  channels = Utils.get_channels_from_segment(filenames, segment_index, block_index)
+
+  # 2. Filter for the channels we're interested in
+  needed_channels = filter(lambda channel: channel['name'] in channel_names, channels)
+
+  # 3. Format from neo to ndi format
+  formatted_channels = list(map(lambda channel: {
+    'internal_type':        channel['_type'],
+    'internal_number':      channel['id'],
+    'internal_channelname': channel['name'],
+    'ndr_type':             Utils.channel_type_from_neo_to_ndr(channel['_type']),
+    # TODO hieroglyph prints out if we don't convert to str
+    'samplerate':           str(Utils.channel_to_sample_rate(channel)),
+    # This is a nonstandard Neo-only field
+    'stream_id':            channel['stream_id']
+  }, needed_channels))
+
+  return formatted_channels
+
+def canbereadtogether(channelstruct):
+  stream_ids = list(map(lambda channel: channel['stream_id'], channelstruct))
+  unique_stream_ids = np.unique(stream_ids)
+
+  if (len(unique_stream_ids) > 1):
+    error_message = ''
+    for channel in channelstruct:
+      error_message += f"\nChannel_id: '{channel['id']}', stream_id: '{channel['stream_id']}'."
+
+    return {
+      'b': 0,
+      'errormsg': f"All of your channels should belong to a single signal_stream in Neo.\n{error_message}\n"
+    }
+  else:
+    return {
+      'b': 1,
+      'errormsg': ""
+    }
+
+def samplerate(filenames, channel_ids):
+  header_channels = Utils.get_header_channels(filenames)
+  our_channels = list(filter(lambda channel: channel['id'] in channel_ids, header_channels))
+  sample_rates = list(map(Utils.channel_to_sample_rate, our_channels))
+  return sample_rates
+
+def t0_t1(filenames, segment_index, block_index=1):
+  reader = neo.io.get_io(filenames[0])
+  block = reader.read()[block_index - 1]
+  segment = block.segments[segment_index - 1]
+
+  def get_magnitude(q):
+    return q.rescale('s').item()
+
+  return [get_magnitude(segment.t_start), get_magnitude(segment.t_stop)]
 
 def read_channel_events(channel_type, channel_ids, filenames, segment_index, t0, t1):
   pass
