fe74a4f6594df26789b848749cbebe3ad6f0ec41
Stephen D. Van Hooser <vanhoosr@brandeis.edu>
2024-04-23 11:09:55 -0400

    updating read_SEV_header

--- +ndr/+format/+tdt/read_SEV_header.m
+++ +ndr/+format/+tdt/read_SEV_header.m
@@ -1,4 +1,4 @@
-function header = read_SEV_header(dirname)
+function header = read_SEV_header(dirname, varargin)
 % READ_SEV_HEADER - read the header and channel information for a TDT directory of files
 %
 % HEADER = READ_SEV_HEADER(DIRNAME)
@@ -14,11 +14,50 @@ end;
 
 	% mimicked from SEV2mat.m by TDT
 
+CHANNEL   = 0;
+EVENTNAME = '';
+DEVICE    = '';
+TANK      = '';
+BLOCK     = '';
+T1        = 0;
+T2        = 0;
+RANGES    = [];
+VERBOSE   = 0;
+JUSTNAMES = 0;
+FS        = 0;
+
+VALID_PARS = {'CHANNEL','EVENTNAME','DEVICE','TANK','BLOCK','T1','T2' ...
+    'RANGES','VERBOSE','JUSTNAMES','FS'};
+
+% parse varargin
+for ii = 1:2:length(varargin),
+    if ~ismember(upper(varargin{ii}), VALID_PARS)
+        error('%s is not a valid parameter. See help SEV2mat.', upper(varargin{ii}));
+    end
+    eval([upper(varargin{ii}) '=varargin{ii+1};']);
+end
+
+if any([~isempty(DEVICE) ~isempty(TANK) ~isempty(BLOCK)])
+    if any([isempty(DEVICE) isempty(TANK) isempty(BLOCK)])
+        error('DEVICE, TANK and BLOCK must all be specified');
+    else
+        dirname = sprintf('\\\\%s\\data\\%s\\%s\\', DEVICE, TANK, BLOCK);
+    end
+end;
+
+data = [];
+sample_info = []; 
+
 ALLOWED_FORMATS = {'single','int32','int16','int8','double','int64'};
 
 JUSTNAMES = 0;
 
 file_list = dir([dirname filesep '*.sev']);
+
+% parse log files
+if JUSTNAMES == 0
+	sample_info = ndr.format.tdt.read_SEV_logs(dirname,'VERBOSE',VERBOSE);
+end
 
 for ii=1:numel(file_list),
 
@@ -43,7 +82,7 @@ for ii=1:numel(file_list),
 	% check file size
 	file_list(ii).data_size = file_list(ii).bytes - 40;
 
-	path = [SEV_DIR file_list(ii).name];
+	path = [dirname filesep file_list(ii).name];
 	fid = fopen(path, 'rb');
 	if fid < 0
 		error([path ' not opened'])
@@ -90,8 +129,7 @@ for ii=1:numel(file_list),
 		%end
 
 		% current channel of stream
-		streamHeader.channelNum        = fread(fid, 1, 'uint16');
-		file_list(ii).chan = streamHeader.channelNum;
+		file_list(ii).chan = fread(fid, 1, 'uint16'); % SDV modification, taking this out of streamHeader
 		% total number of channels in the stream
 		streamHeader.totalNumChannels  = fread(fid, 1, 'uint16');
 		% number of bytes per sample
@@ -103,8 +141,8 @@ for ii=1:numel(file_list),
 		streamHeader.dForm      = ALLOWED_FORMATS{bitand(dform,7)+1};
 
 		% used to compute actual sampling rate
-		streamHeader.decimate   = fread(fid, 1, 'uint8');
-		streamHeader.rate       = fread(fid, 1, 'uint16');
+		decimate   = fread(fid, 1, 'uint8'); % SDV taking this out of the header that is returned
+		rate       = fread(fid, 1, 'uint16'); % SDV taking this out of the header that is returned
 	else
 		error([file_list(ii).name ' has unknown version ' num2str(streamHeader.fileVersion)]);
 	end
@@ -112,7 +150,7 @@ for ii=1:numel(file_list),
 	% compute sampling rate
 	if streamHeader.fileVersion > 0
 		%streamHeader.fs = 2^(streamHeader.rate)*25000000/2^12/streamHeader.decimate;
-		streamHeader.fs = 2^(streamHeader.rate - 12) * 25000000 / streamHeader.decimate;
+		streamHeader.fs = 2^(rate - 12) * 25000000 / decimate;
 	else
 		% make some assumptions if we don't have a real header
 		streamHeader.dForm = 'single';
@@ -146,4 +184,43 @@ for ii=1:numel(file_list),
 		end
 	end
 
-end;
+	% check variable name (workaround for makeValidName support in older Matlab)
+	% varname = matlab.lang.makeValidName(streamHeader.eventName); % newer matlab supports this instead
+	varname = streamHeader.eventName;
+	prepend_x = 0;
+	for jj = 1:numel(varname)
+		% replace bad field characters with '_'
+		if ~isletter(varname(jj)) && isnan(str2double(varname(jj)))
+			varname(jj) = '_';
+		end
+
+		% can't start field name with an underscore or number
+		if jj == 1 && (varname(jj) == '_' || ~isnan(str2double(varname(jj))))
+			prepend_x = 1;
+		end
+	end
+
+	if prepend_x
+		varname = ['x' varname];
+	end
+
+	%fprintf('%s   %s\n', varname, matlab.lang.makeValidName(streamHeader.eventName))
+
+	if ~isvarname(streamHeader.eventName)
+		warning('%s is not a valid Matlab variable name, changing to %s', streamHeader.eventName, varname);
+	end
+
+	func = str2func(streamHeader.dForm);
+	tempvar = func(zeros(1,1));
+	w = whos('tempvar');
+	file_list(ii).itemSize = w.bytes;
+	file_list(ii).npts = file_list(ii).data_size / file_list(ii).itemSize;
+	file_list(ii).fs = streamHeader.fs;
+	file_list(ii).dForm = streamHeader.dForm;
+	file_list(ii).eventName = streamHeader.eventName;
+	file_list(ii).varName = varname;
+	fclose(fid);
+
+end; % loop over file_list
+
+header = file_list;
